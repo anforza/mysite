579b18c2fef6e8f20667af837fcc93064421e8ca
anforza <mr.rustamkhonov@gmail.com>
2026-06-01 14:46:15 +0500

    Delete sayt directory

--- sayt/src/components/SiteFooter.tsx
+++
@@ -1,9 +0,0 @@
-export function SiteFooter() {
-  return (
-    <footer className="mt-16 border-t bg-white">
-      <div className="container-page py-6 text-sm text-slate-600">
-        <p>© {new Date().getFullYear()} EDU PORTAL. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
